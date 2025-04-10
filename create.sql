118682ef5af8423f2f8d4149ae1bcbc1e7e8eee5
github yosshi-kataoka <github>
2025-04-11 06:08:52 +0900

    データベース関連の責務を担うDatabaseManager.phpを作成。

---
+++ create.sql
@@ -0,0 +1,28 @@
+DROP DATABASE  IF EXISTS shuffle_lunch_service;
+CREATE DATABASE IF NOT EXISTS shuffle_lunch_service;
+
+USE shuffle_lunch_service;
+
+DROP TABLE IF EXISTS
+  id,
+  name;
+
+CREATE TABLE list_employees (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(100) NOT NULL
+);
+
+LOAD DATA
+  INFILE '/var/lib/mysql-files/listEmployees.txt'
+
+INTO TABLE
+  list_employees
+  FIELDS TERMINATED BY ' '
+  LINES TERMINATED BY '\n'
+  (@col1)
+
+SET
+  name = @col1;
+
+UPDATE
+  list_employees SET name = REPLACE(name, "'", "");
